b83d7c8172c4ee365651a0b9f7a026e219cded90
jicint <117445339+jicint@users.noreply.github.com>
2023-03-14 11:22:37 +0100

    Add files via upload

---
+++ home.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+
+    <!--super swipe css link-->
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"/>
+
+    <!--fontawesome cdn link-->
+    <link rel="stylesheet"href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"/>
+
+    <link rel="stylesheet" href="style.css"/>
+
+
+    <!--custom js-->
+
+</head>
+   
+<body>
+
+<!--header section-->
+    <section class="header">
+        <a href="home.php" class="logo">travel.</a>
+
+        <nav class="navbar">
+            <a href="home.php">home</a>
+            <a href="about.php">about</a>
+            <a href="package.php">package</a>
+            <a href="book_form.php">book</a>
+        </nav>
+        <div id="menu-btn" class="fas fa-bars"></div>
+    </section>
+    <!-- header section ends -->
+
+    <!-- header section starts-->
+
+    <section class="home">
+        <div class="swiper home-slider">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide slide" style="background:url(images/home-slide-1.jpg)no-repeat">
+                    <div class="content">
+                        <span>explore,discover,travel</span>
+                        <h3>travel around the world</h3>
+                        <a href="package.php" class="btn">discover more</a>
+                    </div>
+                </div>
+
+                <div class="swiper-slide slide" style="background:url(images/home-slide-2.jpg)no-repeat">
+                    <div class="content">
+                        <span>explore,discover,travel</span>
+                        <h3>travel around the new places</h3>
+                        <a href="package.php" class="btn">discover more</a>
+                    </div>
+                </div>
+
+                <div class="swiper-slide slide" style="background:url(images/home-slide-3.jpg)no-repeat">
+                    <div class="content">
+                        <span>explore,discover,travel</span>
+                        <h3>make your tour memorable</h3>
+                        <a href="package.php" class="btn">discover more</a>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div>
+        
+    </section>
+
+    <!--header section ends -->
+    
+    <!-- services section-->
+    <section class="services">
+        <h1 class="heading-title">Our services</h1>
+        <div class="box-container">
+            <div class="box">
+                <img src="images/icon-1.png" alt="">
+                <h3>adventure</h3>
+            </div>
+
+            <div class="box">
+                <img src="images/icon-2.png" alt="">
+                <h3>tour guide</h3>
+            </div>
+
+            <div class="box">
+                <img src="images/icon-3.png" alt="">
+                <h3>trekking</h3>
+            </div>
+
+            <div class="box">
+                <img src="images/icon-4.png" alt="">
+                <h3>camp fire</h3>
+            </div>
+
+            <div class="box">
+                <img src="images/icon-5.png" alt="">
+                <h3>off road</h3>
+            </div>
+
+            <div class="box">
+                <img src="images/icon-6.png" alt="">
+                <h3>camping</h3>
+            </div>
+        </div>
+</section>
+
+
+
+<!--service section ends-->
+<!--home about section starts-->
+
+<section class="home-about">
+    <div class="image">
+        <img src="images/about-img.jpg" alt="">
+    </div>
+    <div class="content">
+        <h3>About us</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita et, recusandae nobis fugit modi quibusdam ea assumenda, nulla quisquam repellat rem aliquid sequi maxime sapiente autem ipsum? Nobis, provident voluptate?</p>
+        <a href="about.php" class="btn">Read more</a>
+    </div>
+</section>
+<!--home about section ends -->
+<!-- home packages section -->
+
+<section class="home-packages">
+    <h1 class="heading-title">Our packages</h1>
+        <div class="box-container">
+            <div class="box">
+                <div class="image">
+                    <img src="images/img-1.jpg"alt="">
+                </div>
+                <div class="content">
+                    <h3>adventure & tour</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita et, recusandae nobis fugit modi quibusdam ea assumenda, nulla quisquam repellat rem aliquid sequi maxime sapiente autem ipsum? Nobis, provident voluptate?</p>
+                    <a href="book-form.php" class="btn">Book now</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="image">
+                    <img src="images/img-2.jpg"alt="">
+                </div>
+                <div class="content">
+                    <h3>adventure & tour</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita et, recusandae nobis fugit modi quibusdam ea assumenda, nulla quisquam repellat rem aliquid sequi maxime sapiente autem ipsum? Nobis, provident voluptate?</p>
+                    <a href="book-form.php" class="btn">Book now</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="image">
+                    <img src="images/img-3.jpg"alt="">
+                </div>
+                <div class="content">
+                    <h3>adventure & tour</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita et, recusandae nobis fugit modi quibusdam ea assumenda, nulla quisquam repellat rem aliquid sequi maxime sapiente autem ipsum? Nobis, provident voluptate?</p>
+                    <a href="book-form.php" class="btn">Book now</a>
+                </div>
+            </div>
+        </div>
+    <div class="load-more"><a href="package.php" class="btn">Load more</a></div>
+</section>
+<!-- home packages section ends -->
+
+<!-- home offer section starts -->
+
+<section class="home-offer">
+    <div class="content">
+        <h3>upto 50% off</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita et, recusandae nobis fugit modi quibusdam ea assumenda, nulla quisquam repellat rem aliquid sequi maxime sapiente autem ipsum? Nobis, provident voluptate?</p>
+        <a href="book.php" class="btn">Book now</a>
+    </div>
+</section>
+<!--home offer section ends -->
+
+<!-- home footer section -->
+
+    <section class="footer">
+        <div class="box-container">
+            <div class="box">
+                <h3>quick links</h3>
+                <a href="home.php"><i class="fas fa-angle-right"></i>home</a>
+                <a href="about.php"><i class="fas fa-angle-right"></i>about</a>
+                <a href="package.php"><i class="fas fa-angle-right"></i>package</a>
+                <a href="book_form.php"><i class="fas fa-angle-right"></i>book</a>
+            </div>
+
+            <div class="box">
+                <h3>extra links</h3>
+                <a href="home.php"><i class="fas fa-angle-right"></i>ask questions</a>
+                <a href="about.php"><i class="fas fa-angle-right"></i>about us</a>
+                <a href="package.php"><i class="fas fa-angle-right"></i>privacy policy</a>
+                <a href="book_form.php"><i class="fas fa-angle-right"></i>terms of use</a>
+            </div>
+
+            <div class="box">
+                <h3>contact info</h3>
+                <a href="#"><i class="fas fa-phone"></i>+123-456-7890</a>
+                <a href="#"><i class="fas fa-phone"></i>+111-222-3333</a>
+                <a href="#"><i class="fas fa-envelope"></i>jicinthekk@gmail.com</a>
+                <a href="#"><i class="fas fa-map"></i>puchheim, germany-82178</a>
+            </div>
+
+            <div class="box">
+                <h3>follow us</h3>
+                <a href="#"><i class="fab fa-facebook"></i>facebook</a>
+                <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+                <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+                <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+            </div>
+        </div>
+
+    <div class="credit">Created by<span> Ms.Jicin Thekkekara</span> | all rights reserved</div>
+    </section>
+
+
+    
+    <!-- swiper js script-->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+    
+    <script src="script.js"></script>
+
+</body>
+</html>
